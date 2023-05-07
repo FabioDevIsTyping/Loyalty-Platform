8f26e75de502f0b4b5e246d70396ed48ae112871
FabioDevIsTyping <131898054+FabioDevIsTyping@users.noreply.github.com>
2023-05-07 10:20:58 +0200

    Code Started

---
+++ mvn/src/main/java/projectIDS/dmd/DmdApplication.java
@@ -0,0 +1,13 @@
+package projectIDS.dmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmdApplication.class, args);
+	}
+
+}
---
+++ mvn/src/main/java/projectIDS/dmd/model/Client.java
@@ -0,0 +1,61 @@
+package projectIDS.dmd.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cliente")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    public Long getId(){
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // costruttori, getter e setter
+}
