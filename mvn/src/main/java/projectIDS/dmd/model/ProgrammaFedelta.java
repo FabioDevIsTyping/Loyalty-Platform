7d0931b2ce1f97e0a803a3139594f804b3cf05cb
FabioDevIsTyping <131898054+FabioDevIsTyping@users.noreply.github.com>
2023-05-08 14:12:14 +0200

    updated programmafedelta class

--- mvn/src/main/java/projectIDS/dmd/model/ProgrammaFedelta.java
+++ mvn/src/main/java/projectIDS/dmd/model/ProgrammaFedelta.java
@@ -1,18 +1,46 @@
 package projectIDS.dmd.model;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table (name="programmaFedelta")
 public class ProgrammaFedelta {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY);
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private int id;
-    private String 
+    private String nomeProgramma;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "puntovendita_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+
+    public int getId()
+    {
+        return this.id;
+    }
+
+    public String getNomeProgramma()
+    {
+        return this.nomeProgramma;
+    }
+
+    public void setNomeProgramma(String newNome)
+    {
+        this.nomeProgramma=newNome;
+    }
+
+    
+
+    
     
 }
