484043f40f79430b4dfcb36d557e73eaccf3117d
FabioDevIsTyping <131898054+FabioDevIsTyping@users.noreply.github.com>
2023-05-11 21:23:33 +0200

    Code setup changes

---
+++ mvn/src/main/java/projectIDS/dmd/repository/CartaFedeltaRepository.java
@@ -0,0 +1,8 @@
+package projectIDS.dmd.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import projectIDS.dmd.model.CartaFedelta;
+
+public interface CartaFedeltaRepository extends JpaRepository<CartaFedelta,Integer>{
+    
+}
