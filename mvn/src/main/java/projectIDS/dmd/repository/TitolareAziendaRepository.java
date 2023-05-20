d92b4b615902bfac4e196020dc26d10d16450736
FabioDevIsTyping <131898054+FabioDevIsTyping@users.noreply.github.com>
2023-05-20 11:36:19 +0200

    Added TitolareAzienda class
    and his repository and controller.

---
+++ mvn/src/main/java/projectIDS/dmd/repository/TitolareAziendaRepository.java
@@ -0,0 +1,9 @@
+package projectIDS.dmd.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import projectIDS.dmd.model.TitolareAzienda;
+
+public interface TitolareAziendaRepository extends JpaRepository<TitolareAzienda,Integer>{
+    
+}
