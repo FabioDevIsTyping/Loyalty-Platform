e1ffb109f85f9c30e2db9872949bcb8082e3a041
FabioDevIsTyping <131898054+FabioDevIsTyping@users.noreply.github.com>
2023-05-23 15:37:35 +0200

    Changed elementCollection to ManyToMany

--- mvn/src/main/java/projectIDS/dmd/model/ComunicazionePromozionale.java
+++ mvn/src/main/java/projectIDS/dmd/model/ComunicazionePromozionale.java
@@ -13,6 +13,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
@@ -36,7 +37,7 @@ public class ComunicazionePromozionale {
     @JoinColumn(name = "TipoComunicazionePromozionale_id")
     private TipoComunicazionePromozionale tipoComunicazionePromozionale;
 
-    @ElementCollection
+    @ManyToMany
     private List<Client> clientiDestinatari;
 
     private LocalDateTime orarioDiInvio;
