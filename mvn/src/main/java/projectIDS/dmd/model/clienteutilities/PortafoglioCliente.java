394b13abfb3cf92cfe1657a3018be9a96e9ea123
Matteo Caggiano <matteo.caggiano@studenti.unicam.it>
2023-06-01 00:43:30 +0200

    [APITEST] Aggiunti commenti JAVADOC + Correzione codice per PortafoglioCliente.java

--- mvn/src/main/java/projectIDS/dmd/model/clienteutilities/PortafoglioCliente.java
+++ mvn/src/main/java/projectIDS/dmd/model/clienteutilities/PortafoglioCliente.java
@@ -14,110 +14,137 @@ import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import projectIDS.dmd.model.persone.Client;
 
+/**
+ * Rappresenta il portafoglio di un cliente nel sistema.
+ *
+ * La classe rappresenta il portafoglio di un cliente e contiene informazioni come l'ID (chiave primaria), il cliente associato,
+ * le carte fedeltà associate, il numero di carte, e l'ultima data e ora di aggiornamento del portafoglio.
+ * 
+ * L'identificatore unico per un portafoglio è l'id, che viene generato
+ * automaticamente attraverso una strategia di generazione.
+ * 
+ * La classe è annotata con @Entity per indicare che è una classe persistente e
+ * può essere salvata nel database. La tabella associata a questa classe nel 
+ * database è denominata "Portafogli" e viene specificato utilizzando 
+ * l'annotazione @Table.
+ */
 @Entity
 @Table(name = "Portafogli")
 public class PortafoglioCliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    
+
+    /**
+     * Il cliente associato al portafoglio.
+     */
     @OneToOne
     @JoinColumn(name = "client_id")
     private Client client;
-    
+
+    /**
+     * Le carte fedeltà associate al portafoglio.
+     */
     @OneToMany(mappedBy = "portafoglioCliente", cascade = CascadeType.MERGE, orphanRemoval = true)
     private List<CartaFedelta> carteFedelta;
-    
-    private int numeroCarte;
-    private LocalDateTime ultimoAggiornamento;
-    
+
     /**
-     * Returns the unique identifier of this wallet.
+     * Il numero di carte fedeltà associate al portafoglio.
+     */
+    private int numeroCarte;
+
+    /**
+     * L'ultima data e ora di aggiornamento del portafoglio.
+     */
+    private LocalDateTime ultimoAggiornamento;
+
+    /**
+     * Restituisce l'identificatore unico di questo portafoglio.
      *
-     * @return the identifier of this wallet
+     * @return l'identificatore di questo portafoglio
      */
     public int getId()
     {
         return this.id;
     }
-    
+
     /**
-     * Returns the client associated with this wallet.
+     * Restituisce il cliente associato a questo portafoglio.
      *
-     * @return the client associated with this wallet
+     * @return il cliente associato a questo portafoglio
      */
     public Client getClient()
     {
         return this.client;
     }
-    
+
     /**
-     * Sets the client associated with this wallet.
+     * Imposta il cliente associato a questo portafoglio.
      *
-     * @param newClient the new client to associate with this wallet
+     * @param newClient il nuovo cliente da associare a questo portafoglio
      */
     public void setClient(Client newClient)
     {
         this.client = newClient;
     }
-    
+
     /**
-     * Returns the list of loyalty cards associated with this wallet.
+     * Restituisce la lista delle carte fedeltà associate a questo portafoglio.
      *
-     * @return the list of loyalty cards associated with this wallet
+     * @return la lista delle carte fedeltà associate a questo portafoglio
      */
     public List<CartaFedelta> getCarteFedelta()
     {
         return this.carteFedelta;
     }
-    
+
     /**
-     * Sets the list of loyalty cards associated with this wallet.
+     * Imposta la lista delle carte fedeltà associate a questo portafoglio.
      *
-     * @param newListaCarte the new list of loyalty cards to associate with this wallet
+     * @param newListaCarte la nuova lista delle carte fedeltà da associare a questo portafoglio
      */
     public void setCarteFedelta(List<CartaFedelta> newListaCarte){
         this.carteFedelta = newListaCarte;
     }
-    
+
     /**
-     * Returns the number of loyalty cards associated with this wallet.
+     * Restituisce il numero di carte fedeltà associate a questo portafoglio.
      *
-     * @return the number of loyalty cards associated with this wallet
+     * @return il numero di carte fedeltà associate a questo portafoglio
      */
     public int getNumeroCarte()
     {
         return this.numeroCarte;
     }
-    
+
     /**
-     * Sets the number of loyalty cards associated with this wallet.
+     * Imposta il numero di carte fedeltà associate a questo portafoglio.
      *
-     * @param newNumeroCarte the new number of loyalty cards to associate with this wallet
+     * @param newNumeroCarte il nuovo numero di carte fedeltà da associare a questo portafoglio
      */
     public void setNumeroCarte(int newNumeroCarte)
     {
         this.numeroCarte=newNumeroCarte;
     }
-    
+
     /**
-     * Returns the date and time of the last update made to this wallet.
+     * Restituisce la data e ora dell'ultimo aggiornamento effettuato su questo portafoglio.
      *
-     * @return the date and time of the last update made to this wallet
+     * @return la data e ora dell'ultimo aggiornamento effettuato su questo portafoglio
      */
     public LocalDateTime getUltimoAggiornamento()
     {
         return this.ultimoAggiornamento;
     }
-    
+
     /**
-     * Sets the date and time of the last update made to this wallet.
+     * Imposta la data e ora dell'ultimo aggiornamento effettuato su questo portafoglio.
      *
-     * @param newUltimoAggiornamento the new date and time of the last update to associate with this wallet
+     * @param newUltimoAggiornamento la nuova data e ora dell'ultimo aggiornamento da associare a questo portafoglio
      */
     public void setUltimoAggiornamento(LocalDateTime newUltimoAggiornamento)
     {
         this.ultimoAggiornamento=newUltimoAggiornamento;
     }
-    
 }
+
