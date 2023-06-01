3f9a343c4ca9a6b36583ec3b16208698875b5490
FabioDevIsTyping <131898054+FabioDevIsTyping@users.noreply.github.com>
2023-06-01 22:24:10 +0200

    Fixed comments and format

--- mvn/src/main/java/projectIDS/dmd/model/clienteutilities/Acquisto.java
+++ mvn/src/main/java/projectIDS/dmd/model/clienteutilities/Acquisto.java
@@ -14,115 +14,126 @@ import jakarta.persistence.Table;
 import projectIDS.dmd.model.persone.Client;
 import projectIDS.dmd.model.puntovenditautilities.PuntoVendita;
 
-
 /**
  * Rappresenta un acquisto nel sistema.
  *
- * La classe rappresenta un acquisto e contiene informazioni come l'ID (chiave primaria), l'importo dell'acquisto
+ * La classe rappresenta un acquisto e contiene informazioni come l'ID (chiave
+ * primaria), l'importo dell'acquisto
  * e la data dell'acquisto.
  * 
  * L'identificatore unico per un acquisto è l'id, che viene generato
  * automaticamente attraverso una strategia di generazione.
  * 
  * La classe è annotata con @Entity per indicare che è una classe persistente e
- * può essere salvata nel database. La tabella associata a questa classe nel 
- * database è denominata "Acquisti" e viene specificato utilizzando 
- * l'annotazione @Table. 
+ * può essere salvata nel database. La tabella associata a questa classe nel
+ * database è denominata "Acquisti" e viene specificato utilizzando
+ * l'annotazione @Table.
  * 
- * Infine l'annotazione @JsonIgnore è stata introdotta per indicare che la relazione con l'attributo puntoVendita
- * deve essere ignorata durante la serializzazione in formato Json, in questo modo non si vanno a creare loop infiniti
+ * Infine l'annotazione @JsonIgnore è stata introdotta per indicare che la
+ * relazione con l'attributo puntoVendita
+ * deve essere ignorata durante la serializzazione in formato Json, in questo
+ * modo non si vanno a creare loop infiniti
  * dovuti alla relazione many to many quando si cercherà di effettuare una get.
  * 
  */
-@Entity 
-@Table (name="Acquisti")
+@Entity
+@Table(name = "Acquisti")
 public class Acquisto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private double importoAcquisto;
     private Date dataAcquisto;
-    
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private Client client;
-    
+
     @ManyToOne
-    @JoinColumn(name = "puntoVendita_id" , nullable = false)
+    @JoinColumn(name = "puntoVendita_id", nullable = false)
     @JsonIgnore
     private PuntoVendita puntoVendita;
 
     /**
-     * Returns the ID of this purchase.
+     * Restituisce l'ID di questo acquisto.
      *
-     * @return the ID of this purchase.
+     * @return l'ID di questo acquisto.
      */
     public int getId() {
         return this.id;
     }
 
     /**
-     * Returns the purchase amount.
+     * Restituisce l'importo dell'acquisto.
      *
-     * @return the purchase amount.
+     * @return l'importo dell'acquisto.
      */
     public double getImportoAcquisto() {
         return this.importoAcquisto;
     }
 
     /**
-     * Sets the purchase amount.
+     * Imposta l'importo dell'acquisto.
      *
-     * @param newImporto the new purchase amount.
+     * @param newImporto il nuovo importo dell'acquisto.
      */
     public void setImportoAcquisto(double newImporto) {
         this.importoAcquisto = newImporto;
     }
 
     /**
-     * Returns the date of the purchase.
+     * Restituisce la data dell'acquisto.
      *
-     * @return the date of the purchase.
+     * @return la data dell'acquisto.
      */
     public Date getDataAcquisto() {
         return this.dataAcquisto;
     }
 
     /**
-     * Sets the date of the purchase.
+     * Imposta la data dell'acquisto.
      *
-     * @param newDate the new date of the purchase.
+     * @param newDate la nuova data dell'acquisto.
      */
     public void setDataAcquisto(Date newDate) {
         this.dataAcquisto = newDate;
     }
 
-   
+    /**
+     * Restituisce il cliente che ha effettuato l'acquisto.
+     *
+     * @return il cliente che ha effettuato l'acquisto.
+     */
     public Client getClient() {
         return client;
     }
 
+    /**
+     * Imposta il cliente che ha effettuato l'acquisto.
+     *
+     * @param client il nuovo cliente che ha effettuato l'acquisto.
+     */
     public void setClient(Client client) {
         this.client = client;
     }
 
     /**
-     * Returns the store where the purchase was made.
+     * Restituisce il punto vendita in cui è stato effettuato l'acquisto.
      *
-     * @return the store where the purchase was made.
+     * @return il punto vendita in cui è stato effettuato l'acquisto.
      */
     public PuntoVendita getPuntoVendita() {
         return this.puntoVendita;
     }
 
     /**
-     * Sets the store where the purchase was made.
+     * Imposta il punto vendita in cui è stato effettuato l'acquisto.
      *
-     * @param newPuntoVendita the new store where the purchase was made.
+     * @param newPuntoVendita il nuovo punto vendita in cui è stato effettuato
+     *                        l'acquisto.
      */
     public void setPuntoVendita(PuntoVendita newPuntoVendita) {
         this.puntoVendita = newPuntoVendita;
     }
 
 }
-
